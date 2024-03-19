831252ad7d6d4d63969519989e2d3ecc67b31560
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-03-19 07:36:43 -0400

    created a shape class that uses a constructor to set the colors

---
+++ library/shapes.js
@@ -0,0 +1,14 @@
+//This class uses a constructor to assign the color
+class shape{
+    constructor(){
+        this.color=''
+    }
+}
+
+setColor(color){
+    this.color=(color);
+    }
+
+   
+
+
