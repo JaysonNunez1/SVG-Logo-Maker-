bb480be3da092f77b7cfb54c2e3f33eb847fcd0c
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-03-22 07:35:33 -0400

    created circle class that extends shape and renders it based on users input

--- library/shapes.js
+++ library/shapes.js
@@ -3,15 +3,23 @@ class shape{
     constructor(){
         this.color=''
     }
-}
 
-setColor(color){
-    this.color=(color);
+
+setColor(color) {
+    this.color= (color);
     }
+}
 
 class Square extends shape{
     render(){
         return `<rect x="50" y="35" width="200" height="200" fill= "${this.color}" />`
+    }
+}
+
+class Circle extends shape{
+    render(){
+        return  ` <circle cx="70% " cy="70%" r="150" height="150%" width="150%"
+        fill= "${this.color}" />`;
     }
 }
 
