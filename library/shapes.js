91d4df8d7c3acabe21e0df29b716400de66951d9
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-03-22 07:22:10 -0400

    created a square class that extends the shape and renders based on user input

--- library/shapes.js
+++ library/shapes.js
@@ -9,6 +9,13 @@ setColor(color){
     this.color=(color);
     }
 
+class Square extends shape{
+    render(){
+        return `<rect x="50" y="35" width="200" height="200" fill= "${this.color}" />`
+    }
+}
+
+
+    
+
    
-
-
